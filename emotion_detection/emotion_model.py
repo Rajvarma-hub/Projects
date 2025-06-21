43900d19b28415e746e9ae93357b8c3da966925f
Rajvarma-hub <rajkumarthirthala2005@gmail.com>
2025-06-21 13:55:58 +0530

    Update emotion_model.py

--- emotion_detection/emotion_model.py
+++ emotion_detection/emotion_model.py
@@ -1,12 +1,13 @@
 from huggingface_hub import InferenceClient
 from dotenv import load_dotenv
+import streamlit as st
 import os
 load_dotenv()
 api_token=os.getenv("api_key")
 
 def emotion(text):
     model = "j-hartmann/emotion-english-distilroberta-base"
-    client=InferenceClient(token=api_token)
+    client=InferenceClient(token=st.secrets['api_key'])
     reponse=client.text_classification(text=text,model=model)
     sorted_emotions=max(reponse,key=lambda x:x['score'])
     return sorted_emotions
